80fed331f847d3fa21075cf0e7b05a5bac723337
Ubuntu <saurabh.singh@vnnogile.in>
2023-01-07 15:27:38 +0530

    Updates routes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hii")
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+app.listen(port, () => {
+    console.log(`Server is listening to request at port ${port}`);
+});
---
+++ routes/employeeRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get('/api/v1/employee', );
+
+module.exports = router;
